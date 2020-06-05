665c508007a2a989bc9bb83c9da33000698f5a99
Alex Ye <alexzye1@gmail.com>
2020-06-05 00:58:35 -0700

    i see u there

--- whateat/__main__.py
+++ whateat/__main__.py
@@ -1,13 +1,25 @@
 import sys
 import uuid
 from .api import yelp
+from .api import geolocation
 import geocoder
+
 
 
 def main():
     # uses this inaccurate api as test
     g = geocoder.ip('me')
-    print(g.latlng)
+    # print(g.latlng)
+
+    print(geolocation.geolocate())
+
+
+
+    # geo locate me
+
+    # try google geolocation API first. 
+
+
 
     # stupid way of getting a mac address
     # mac_address = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1])
